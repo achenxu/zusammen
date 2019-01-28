b9342c85c0e99493dcc7803c2e1fbfd447c4d4e6
TildeHacker <contact@tildehacker.com>
2019-01-28 20:22:46 +0100

    Initial commit

---
+++ src/main/java/tn/zusammen/zusammen/ZusammenApplication.java
@@ -0,0 +1,13 @@
+package tn.zusammen.zusammen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZusammenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZusammenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/zusammen/zusammen/ZusammenApplicationTests.java
@@ -0,0 +1,16 @@
+package tn.zusammen.zusammen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZusammenApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

17905a208a9d967348751cbf2cf3768e455da956
TildeHacker <contact@tildehacker.com>
2019-02-05 21:27:39 +0100

    Add findByEmail method for DefaultUserDetailsService
    Signed-off-by: TildeHacker <contact@tildehacker.com>

--- src/main/java/tn/zusammen/zusammen/repositories/UserRepository.java
+++ src/main/java/tn/zusammen/zusammen/repositories/UserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import tn.zusammen.zusammen.entities.User;
 
 public interface UserRepository extends MongoRepository<User, String> {
+    User findByEmail(String email);
 }

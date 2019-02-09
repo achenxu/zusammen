56a6882b2ce966f5743217a6d2ca009e1a4b6e9e
TildeHacker <contact@tildehacker.com>
2019-02-09 22:02:12 +0100

    Add thymeleaf-layout-dialect Bean
    Signed-off-by: TildeHacker <contact@tildehacker.com>

--- src/main/java/tn/zusammen/zusammen/configs/MvcConfig.java
+++ src/main/java/tn/zusammen/zusammen/configs/MvcConfig.java
@@ -1,5 +1,7 @@
 package tn.zusammen.zusammen.configs;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,6 +12,11 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
     }
 
 }

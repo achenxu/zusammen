ae5fe25ec674d6b8d907bab8865854f21a3d5531
TildeHacker <contact@tildehacker.com>
2019-03-26 22:13:52 +0100

    Redesign entities
    - Entities in this commit match diagrams in
    commit 7c95ddceeebc6b58cdcbd7f89232b9a83a23a563.
    - Enums are in a separate package and are not persisted.
    
    Signed-off-by: TildeHacker <contact@tildehacker.com>

--- src/main/java/tn/zusammen/zusammen/entities/ProfilVoyageur.java
+++
@@ -1,7 +0,0 @@
-package tn.zusammen.zusammen.entities;
-
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document(collection="profilsVoyageurs")
-public class ProfilVoyageur {
-}

ff77821bd7a1df36e97e216c17f2fbaecb7f644a
TildeHacker <contact@tildehacker.com>
2019-03-26 22:24:41 +0100

    Delete repositories made for old entities
    Signed-off-by: TildeHacker <contact@tildehacker.com>

--- src/main/java/tn/zusammen/zusammen/repositories/ProfilClientRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.zusammen.zusammen.repositories;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import tn.zusammen.zusammen.entities.ProfilClient;
-
-public interface ProfilClientRepository extends MongoRepository<ProfilClient, String> {
-}

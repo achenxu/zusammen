feff588832b65454205073ffa58bec5b63aae2d7
TildeHacker <contact@tildehacker.com>
2019-03-26 22:31:15 +0100

    Add BCrypt password encoder to authentication provider
    Signed-off-by: TildeHacker <contact@tildehacker.com>

--- src/main/java/tn/zusammen/zusammen/configs/WebSecurityConfig.java
+++ src/main/java/tn/zusammen/zusammen/configs/WebSecurityConfig.java
@@ -1,13 +1,23 @@
 package tn.zusammen.zusammen.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import tn.zusammen.zusammen.services.DefaultUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DefaultUserDetailsService defaultUserDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -27,6 +37,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authProvider());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(defaultUserDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 
 }

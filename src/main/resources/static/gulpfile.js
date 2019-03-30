b04b1194f5b0c5f6d41442163a56c7c5b9b868e4
TildeHacker <contact@tildehacker.com>
2019-03-30 16:35:49 +0100

    Add Leaflet and tempusdominus-bootstrap-4
    Signed-off-by: TildeHacker <contact@tildehacker.com>

--- src/main/resources/static/gulpfile.js
+++ src/main/resources/static/gulpfile.js
@@ -18,6 +18,8 @@ gulp.task('build', function() {
     runSequence(
         'clean:vendors',
         'copyRecursiveVendorFiles',
+        'copyRecursiveLeafletFiles',
+        'copyRecursiveFontAwesomeFiles',
         'buildBaseVendorStyles',
         'buildBaseVendorScripts',
         'buildOptionalVendorScripts',
@@ -36,19 +38,37 @@ gulp.task('copyRecursiveVendorFiles', function() {
         .pipe(gulp.dest('./vendors/iconfonts/mdi'));
 });
 
+gulp.task('copyRecursiveLeafletFiles', function() {
+    return gulp.src(['./node_modules/leaflet/dist/images/*'])
+        .pipe(gulp.dest('./vendors/css/images'));
+});
+
+gulp.task('copyRecursiveFontAwesomeFiles', function() {
+    return gulp.src(['./node_modules/font-awesome/fonts/*'])
+        .pipe(gulp.dest('./vendors/fonts'));
+});
+
 gulp.task('buildBaseVendorScripts', function() {
     return gulp.src([
         './node_modules/jquery/dist/jquery.min.js', 
         './node_modules/popper.js/dist/umd/popper.min.js', 
         './node_modules/bootstrap/dist/js/bootstrap.min.js', 
-        './node_modules/perfect-scrollbar/dist/perfect-scrollbar.min.js'
+        './node_modules/perfect-scrollbar/dist/perfect-scrollbar.min.js',
+        './node_modules/leaflet/dist/leaflet.js',
+        './node_modules/moment/min/moment.min.js',
+        './node_modules/tempusdominus-bootstrap-4/build/js/tempusdominus-bootstrap-4.js'
     ])
       .pipe(concat('vendor.bundle.base.js'))
       .pipe(gulp.dest('./vendors/js'));
 });
 
 gulp.task('buildBaseVendorStyles', function() {
-    return gulp.src(['./node_modules/perfect-scrollbar/css/perfect-scrollbar.css'])
+    return gulp.src([
+        './node_modules/perfect-scrollbar/css/perfect-scrollbar.css',
+        './node_modules/leaflet/dist/leaflet.css',
+        './node_modules/font-awesome/css/font-awesome.css',
+        './node_modules/tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.css'
+    ])
       .pipe(concat('vendor.bundle.base.css'))
       .pipe(gulp.dest('./vendors/css'));
 });
